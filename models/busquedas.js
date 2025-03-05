ac9114090b3d2b54423f0243fd748da861ee095e
César Alejandro Dávila Reyna <126361906+Sxnse1@users.noreply.github.com>
2025-03-05 12:01:42 -0600

    finished

--- models/busquedas.js
+++ models/busquedas.js
@@ -1,12 +1,23 @@
+const fs = require('fs');
 const axios = require('axios');
 require('dotenv').config()
 
 class Busquedas {
 
-    historial = ['Tegucigalpa', 'Madrid', 'San José'];
+    historial = [];
+    dbPath = './db/database.json';
 
     constructor() {
+        this.leerDB();
+    }
 
+    get historialCapitalizado(){
+        return this.historial.map( lugar => {
+            let palabras = lugar.split(' ');
+            palabras = palabras.map(p => p[0].toUpperCase() + p.substring(1));
+
+            return palabras.join(' ')
+        })
     }
 
     get paramsMapbox() {
@@ -14,6 +25,15 @@ class Busquedas {
             'proximity': 'ip',
             'language': 'es',
             'access_token': process.env.MAPBOX_KEY
+        }
+    }
+
+    get paramsWeatherApp() {
+        return {
+
+            appid: process.env.OPENWEATHER_KEY,
+            units: 'metric',
+            lang: 'es'
         }
     }
 
@@ -27,7 +47,7 @@ class Busquedas {
             });
 
             const resp = await intance.get();
-            return resp.data.features.map( lugar => ({
+            return resp.data.features.map(lugar => ({
                 id: lugar.id,
                 nombre: lugar.place_name_es,
                 lng: lugar.center[0],
@@ -45,6 +65,67 @@ class Busquedas {
         }
 
     }
+
+    async climaLugar(lat, lon) {
+
+        try {
+
+            const intance = axios.create({
+                baseURL: 'https://api.openweathermap.org/data/2.5/weather',
+                params: {
+                    ...this.paramsWeatherApp,
+                    lat,
+                    lon,
+                }
+            });
+
+            const resp = await intance.get();
+            const { weather, main} = resp.data;
+
+            return {
+            desc: weather[0].description,
+            min: main.temp_min,
+            max: main.temp_max,
+            temp: main.temp
+            }
+
+        } catch (error) {
+            console.log(error)
+        }
+
+    }
+
+    agregarHistorial(lugar = ''){
+
+        if(this.historial.includes(lugar.toLocaleLowerCase())){
+            return;
+        }
+
+        this.historial = this.historial.splice(0,5);
+
+        this.historial.unshift(lugar.toLocaleLowerCase());
+        this.guardarDB();
+
+    }
+
+    guardarDB(){
+
+        const payload = {
+            historial: this.historial
+        }
+
+        fs.writeFileSync(this.dbPath, JSON.stringify(payload));
+    }
+
+    leerDB(){
+        if(!fs.existsSync(this.dbPath)) return;
+            
+        const info = fs.readFileSync(this.dbPath, {encoding: 'utf-8'});
+        const data = JSON.parse(info);
+        
+        this.historial = data.historial
+    }
+
 }
 
 module.exports = Busquedas;
